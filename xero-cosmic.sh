e035f3c1fc2bdb850999ffa029dd854e0d1c23b3
DarkXero-dev <steve@techxero.com>
2024-09-27 13:39:46 +0300

    Printer

--- xero-cosmic.sh
+++ xero-cosmic.sh
@@ -111,13 +111,13 @@ main_menu() {
 
   case "$CHOICE" in
     1)
-      clear && install_packages "cosmic linux-headers pacman-contrib xdg-user-dirs wayland-protocols wayland-utils lib32-wayland system76-power"
+      clear && install_packages "cosmic linux-headers pacman-contrib xdg-user-dirs wayland-protocols wayland-utils lib32-wayland system76-power system-config-printer"
       ;;
     2)
-      clear && selective_install "cosmic linux-headers pacman-contrib xdg-user-dirs wayland-protocols wayland-utils lib32-wayland system76-power"
+      clear && selective_install "cosmic linux-headers pacman-contrib xdg-user-dirs wayland-protocols wayland-utils lib32-wayland system76-power system-config-printer"
       ;;
     3)
-      clear && install_packages "cosmic-session-git linux-headers pacman-contrib xdg-user-dirs switcheroo-control xdg-desktop-portal-cosmic-git xorg-xwayland just mold cosmic-edit-git cosmic-files-git cosmic-store-git cosmic-term-git cosmic-wallpapers-git wayland-protocols wayland-utils lib32-wayland system76-power"
+      clear && install_packages "cosmic-session-git linux-headers pacman-contrib xdg-user-dirs switcheroo-control xdg-desktop-portal-cosmic-git xorg-xwayland just mold cosmic-edit-git cosmic-files-git cosmic-store-git cosmic-term-git cosmic-wallpapers-git wayland-protocols wayland-utils lib32-wayland system76-power system-config-printer"
       ;;
     *)
       if [ "$CHOICE" == "" ]; then
